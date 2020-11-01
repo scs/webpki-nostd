5e613193f4ee8b9e5159958ea6658efda3d0fe3b
Alain Brenzikofer <alain.brenzikofer@scs.ch>
2020-11-01 08:31:08 +0100

    cleanup. only keep crypto

--- dockerfile.d/06_wasm.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-echo "*** Initializing WASM build environment"
-/root/.cargo/bin/rustup target add wasm32-unknown-unknown --toolchain ${rust_toolchain}
--- pruntime/app/src/contract_output.rs
+++
@@ -1,9 +0,0 @@
-use serde_json::{Map, Value, Error};
-use attestation::Attestation;
-
-#[derive(Serialize, Deserialize)]
-pub struct ContractOutput {
-    pub output: String,
-    pub nonce: Map<String, Value>,
-    pub attestation: Attestation
-}
